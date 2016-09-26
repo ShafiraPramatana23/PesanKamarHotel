aebc7e7fb6c6fbfcd3af5dd9e1920747cd8d166c
Shafira Pramatana <firapramatana03@gmail.com>
2016-09-26 20:47:06 +0700

    Pemberian script untuk mengoprasikan menu form

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl2037/pesankamarhotel/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl2037/pesankamarhotel/MainActivity.java
@@ -2,12 +2,81 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl2037.pesankamarhotel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etNama;
+    EditText etTL;
+    RadioButton rbL, rbP;
+    CheckBox cbSU, cbD, cbS;
+    Spinner spKamar;
+    TextView tvHas, tvHsl, tvHasil, tvLisah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etTL = (EditText) findViewById(R.id.editTextTL);
+        rbL = (RadioButton) findViewById(R.id.radioButtonLK);
+        rbP = (RadioButton) findViewById(R.id.radioButtonP);
+
+        cbSU = (CheckBox) findViewById(R.id.checkBoxSU);
+        cbD = (CheckBox) findViewById(R.id.checkBoxD);
+        cbS = (CheckBox) findViewById(R.id.checkBoxS);
+
+        spKamar = (Spinner) findViewById(R.id.spinnerKR);
+
+        tvHas = (TextView) findViewById(R.id.textViewHasil1);
+        tvHsl = (TextView) findViewById(R.id.textViewHasil2);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil3);
+        tvLisah = (TextView) findViewById(R.id.textViewHasil4);
+
+        findViewById(R.id.buttonOK).setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View view) {
+                doClick();
+            }
+        });
+    }
+
+    private void doClick() {
+        String nama = etNama.getText().toString();
+        int tahun = Integer.parseInt(etTL.getText().toString());
+        int usia = 2016 - tahun;
+        tvHas.setText(nama + " berusia " + usia + " tahun");
+
+        String hasil = null;
+        if (rbL.isChecked()) {
+            hasil = rbL.getText().toString();
+        } else if (rbP.isChecked()) {
+            hasil = rbP.getText().toString();
+        }
+
+        if (hasil == null) {
+            tvHsl.setText("Belum memilih Status");
+        } else {
+            tvHsl.setText("Jenis Kelamin : " + hasil);
+        }
+
+        String hasill="Tipe Kamar yang dipilih : ";
+        int startlen = hasill.length();
+        if(cbSU.isChecked()) hasill+=cbSU.getText().toString();
+        if(cbD.isChecked()) hasill+=cbD.getText().toString();
+        if(cbS.isChecked()) hasill+=cbS.getText().toString();
+
+        if(hasill.length()==startlen) hasill+="Tidak ada pada Pilihan";
+        tvHasil.setText(hasill);
+
+        tvLisah.setText("Kapasitas Kamar yang dipilih : "+spKamar.getSelectedItem().toString());
+
     }
 }
